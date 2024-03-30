bc845289782f09684564a4a117650a7bda20c534
IsaiahH2004 <ihuska2004@gmail.com>
2024-03-30 15:30:21 -0400

    Pushing navigation

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,48 @@
-import HomeScreen from "./screens/HomeScreen";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen'; // Adjust the path as necessary
+import TasksScreen from './screens/ItemScreen'; // Adjust the path as necessary
+import CameraScreen from './screens/CameraScreen'; // Adjust the path as necessary
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-export default function App() {
+function HomeStack() {
   return (
-    <HomeScreen/>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+  <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+  <Stack.Screen name="Camera" component={CameraScreen} options={{ headerShown: false }}/>
+</Stack.Navigator>
+
   );
 }
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Tasks') {
+            iconName = focused ? 'list' : 'list-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}>
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Tasks" component={TasksScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
 
